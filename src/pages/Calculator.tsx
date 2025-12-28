9699bdf31917f348a07b89444349f3e6569ec325
pawan <neupane1818@gmail.com>
2025-12-28 13:44:13 +0545

    3rd commit of day 1

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,9 @@
+const Calculator = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">Calculator Page</h1>
+    </div>
+  )
+}
+
+export default Calculator;
